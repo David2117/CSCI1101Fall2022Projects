9c66c94db4f1088ec93b4f44ec60933a0ef7df96
David C <Davidg.777@hotmail.com>
2022-11-22 10:25:45 -0600

    Add games data

---
+++ Games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,7 @@
+INSERT INTO sandbox(int16_value, string_value, datetime_value) VALUES (2045, 'helpme', '2003-02-12 11:11:11');
+
+UPDATE sandbox SET int32_value = 7777 WHERE id = 80;
+
+DELETE FROM sandbox WHERE id = 56;
+
+SELECT * FROM sandbox;
